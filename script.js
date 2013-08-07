3cb7a6f4f17ef5abe9f2850cbb768d94d2088421
Davide Pedron <ildede@ymail.com>
2013-08-07 03:36:22 +0200

    modificato:   script.js

--- script.js
+++ script.js
@@ -14,8 +14,6 @@ function createElement(text) {
 };
 
 $(document).ready(function(){
-	//quando pronta, la pagina effttua il check dei cookie e crea eventuali elementi salvati
-	checkCookie();
 			
 	//Funzioni utilizzabili dall'utente
 	$('#add').click(function(){
@@ -23,7 +21,7 @@ $(document).ready(function(){
 			var text = $('#item_to_add').val();
 			$('#item_to_add').val("");
 			var newDiv = createElement(text);
-			$('.content').append(newDiv);
+			$('.container').append(newDiv);
 		}
 	});
 	
@@ -31,45 +29,5 @@ $(document).ready(function(){
 		$(":checked").parent().remove();
 	});
 	
-	$('#save').click(function() {
-		var savedPost= [];
-		$('.list').each(function() {
-			savedPost.push( $(this).html() );
-		});
-		setCookie("savedPost",savedPost,365);
-	});
 	// --Fine funzioni utente
 });
-
-
-
-
-// gestione cookie per salvataggio div
-
-function getCookie(c_name) {
-	var i,x,y,ARRcookies=document.cookie.split(";");
-	for (i=0;i<ARRcookies.length;i++) {
-		x=ARRcookies[i].substr(0,ARRcookies[i].indexOf("="));
-		y=ARRcookies[i].substr(ARRcookies[i].indexOf("=")+1);
-		x=x.replace(/^\s+|\s+$/g,"");
-		if (x==c_name){
-			return unescape(y);
-		}
-	};
-};
-
-function setCookie(c_name,value,exdays) {
-	var exdate=new Date();
-	exdate.setDate(exdate.getDate() + exdays);
-	var c_value=escape(value) + ((exdays==null) ? "" : "; expires="+exdate.toUTCString());
-	document.cookie=c_name + "=" + c_value;
-};
-
-function checkCookie() {
-	var savedPost=getCookie("savedPost");
-	alert(savedPost);
-
-		var newDiv = createElement(savedPost);
-		$('.content').append(newDiv);
-
-};
